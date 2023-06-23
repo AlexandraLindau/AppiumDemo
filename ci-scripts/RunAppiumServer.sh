50e0eab7b9fefa99ead08469a1e238cd6ddcde32
Alexandra <alexandra@intergiro.tech>
2023-06-23 02:02:47 +0200

    Add driver installation

--- ci-scripts/RunAppiumServer.sh
+++ ci-scripts/RunAppiumServer.sh
@@ -2,4 +2,7 @@
 set -ex
 npm install -g appium@next
 appium -v
+appium driver install xcuitest
+appium driver install uiautomator2
+appium driver list --installed
 appium --address 0.0.0.0 --port 4723 --relaxed-security --base-path /wd/hub &
